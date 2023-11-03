d4dfd836416c617f3983cd744a3c358939652a80
khamon <khamon@soleaenergy.com>
2023-11-03 13:26:29 -0400

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gihub.com/khamonsol/forward-auth-test
+
+go 1.21.2
---
+++ main.go
@@ -0,0 +1,26 @@
+package forward_auth_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	headersMap := make(map[string][]string)
+
+	for name, values := range r.Header {
+		headersMap[name] = values
+		fmt.Printf("%s: %s\n", name, values)
+	}
+
+	jsonHeaders, err := json.Marshal(headersMap)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonHeaders)
+}
